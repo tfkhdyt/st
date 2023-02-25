f56534be825b01ec7d8dd3e7b0cbd76d24d1d4c6
tfkhdyt <tfkhdyt@proton.me>
2023-02-25 18:13:24 +0700

    Revert "fix(config): set termname to "xterm-256color""
    This reverts commit 36453c85da09cc11aa50a2cff4311ee77a1b6e73.

--- config.def.h
+++ config.def.h
@@ -132,7 +132,7 @@ const int boxdraw_braille = 0;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "xterm-256color";
+char *termname = "st-256color";
 
 /*
  * spaces per tab
